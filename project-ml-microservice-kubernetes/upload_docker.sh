b8fb8b883b298869a13c2d0afe8173c950603ac2
Hung Tong <hungntong@gmail.com>
2023-03-02 07:50:43 +0000

    Commit

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=skycrafter/mlapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag mlapp $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
